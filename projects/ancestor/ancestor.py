68e9bc276b40dd324e3645e0fd752c89fa201d9f
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-17 14:27:54 +0000

    Removed prints

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -16,22 +16,18 @@ def bfs(graph, sv, ancestors):
     while queue.size() > 0:
         path = queue.dequeue()
         nodes = set(path) - visited
-        # print('vertex?', nodes)
         for vertex in nodes:
             parents = [parent for (parent, child) in ancestors if child == vertex]
             if len(parents) != 0:
                 parents = [min(parents)]
             parentsOfParents = [parent for (parent, child) in ancestors if child in parents]
-            # print('parents?', parents)
-            # print('parentsOfParents?', parentsOfParents, len(parentsOfParents))
-            if len(parentsOfParents) == 0: # and queue.size() == 0
+            if len(parentsOfParents) == 0:
                 return -1 if len(parents) == 0 else min(parents)
             for parent in parents:   
                 new_path = list(path)
                 new_path.append(parent)
                 queue.enqueue(new_path)
             visited.add(vertex)
-
 
 if __name__ == '__main__':
     '''
