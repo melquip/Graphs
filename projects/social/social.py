c72df290b2b12b7232f4bff1206c0d9fc2481eee
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-18 15:20:03 +0000

    Answered question 2

--- projects/social/social.py
+++ projects/social/social.py
@@ -103,6 +103,25 @@ class SocialGraph:
         
         return visited
 
+    def extendedSocialNetwork(self, user_id):
+        paths = self.get_all_social_paths(user_id)
+        totalExtendedUsers = []
+        degreesOfSeparation = []
+        for user in paths:
+            path = paths[user]
+            pathSize = len(path)
+            degreeSeparation = 0
+            if pathSize > 2:
+                degreeSeparation = pathSize - 2
+                for friend in path:
+                    if friend not in totalExtendedUsers and friend is not user_id and friend is not user:
+                        totalExtendedUsers.append(friend)
+            degreesOfSeparation.append(degreeSeparation)
+        
+        
+        averageDegreeOfSeparation = float("{0:.2f}".format(sum(degreesOfSeparation) / len(degreesOfSeparation)))
+        percentageOfOtherUsers = float("{0:.2f}".format(len(totalExtendedUsers) / len(self.users) * 100))
+        return (percentageOfOtherUsers, averageDegreeOfSeparation)
 
 """
 To create 100 users with an average of 10 friends each, how many times would you need to call add_friendship()? Why?
@@ -110,12 +129,24 @@ To create 100 users with an average of 10 friends each, how many times would you
 
 If you create 1000 users with an average of 5 random friends each, what percentage of other users will be in a particular user's extended social network? What is the average degree of separation between a user and those in his/her extended network?
 
+1000 * 5 = 5000 // 2 = 2500
+
+41.5 to 44.2 % of other users in extended social network
+3 to 4 average degree of separation
+
+STRETCH
+
+You might have found the results from question #2 above to be surprising. Would you expect results like this in real life? If not, what are some ways you could improve your friendship distribution model for more realistic results?
+
+If you followed the hints for part 1, your populate_graph() will run in O(n^2) time. Refactor your code to run in O(n) time. Are there any tradeoffs that come with this implementation?
 """
+
 if __name__ == '__main__':
     sg = SocialGraph()
-    sg.populate_graph(10, 2)
+    sg.populate_graph(1000, 5)
     print(sg.friendships)
     # test case
     # sg.friendships = {1: {8, 10, 5}, 2: {10, 5, 7}, 3: {4}, 4: {9, 3}, 5: {8, 1, 2}, 6: {10}, 7: {2}, 8: {1, 5}, 9: {4}, 10: {1, 2, 6}}
     connections = sg.get_all_social_paths(1)
     print(connections)
+    print('percentageOfOtherUsers', sg.extendedSocialNetwork(1))
